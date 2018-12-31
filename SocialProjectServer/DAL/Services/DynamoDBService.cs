37db398c4013f9f7e2f50055f83dff7e22bb4f06
nitzan selwyn <nitzansel@gmail.com>
2018-12-31 19:30:57 +0200

    Adding the basic dynamoDB service for connection

---
+++ SocialProjectServer/DAL/Services/DynamoDBService.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+    class DynamoDBService 
+    {
+        DynamoDBContext dynamoDBContext;
+        AmazonDynamoDBClient dynamoDBClient;
+
+        public DynamoDBService()
+        {
+            dynamoDBClient = new AmazonDynamoDBClient();
+
+            dynamoDBContext = new DynamoDBContext(dynamoDBClient, new DynamoDBContextConfig
+            {
+                ConsistentRead = true,
+                SkipVersionCheck = true
+            
+            });
+        }
+    }
+}
