04202349e696a379b23bd3e357479334829f8933
nitzan selwyn <nitzansel@gmail.com>
2018-12-31 19:20:02 +0200

    starting to setup the dynamoDB in my server

---
+++ SocialProjectServer/Common/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialProjectServer.Models
+{
+    [DynamoDBTable("User")]
+    public class User
+    {
+        [DynamoDBHashKey]
+        public string UserName { get; set; }
+
+        [DynamoDBRangeKey]
+        public string Password { get; set; }
+
+        [DynamoDBProperty]
+        public string WorkPlace { get; set; }
+    }
+}
